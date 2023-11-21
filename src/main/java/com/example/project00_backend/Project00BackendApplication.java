5b3c05f3fc499e7841eb54213f42bfd076bc7a4d
Nighteast <jung946244@naver.com>
2023-11-21 20:05:11 +0900

    initial commit

---
+++ src/main/java/com/example/project00_backend/Project00BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.project00_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project00BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project00BackendApplication.class, args);
+    }
+
+}
